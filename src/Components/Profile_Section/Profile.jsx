feeff4790c65e4a5ed447199c8e166639e859335
syedmaaz9905 <syedmaazalinaqvi@gmail.com>
2024-08-01 23:03:42 +0500

    profile pages additions

--- src/Components/Profile_Section/Profile.jsx
+++ src/Components/Profile_Section/Profile.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './profile.css'
+import ProfileBody from './components/Profile_Body/ProfileBody'
 
 const Profile = () => {
     return (
-        <div className='profileMain'>
-            <div className="profileTitle">
-                <h2>Profile Page</h2>
+        <div>
+            <div className='profileMain'>
+                <div className="profileTitle">
+                    <h2>Profile Page</h2>
+                </div>
+            </div>
+
+
+            <div className='profileBodyMain'>
+                <ProfileBody />
             </div>
         </div>
     )
